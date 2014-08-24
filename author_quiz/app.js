95b6266f4cbe71eb1003a35a4f0868fc0bc011f7
stefanRitter <stefan@stefanritter.com>
2014-08-24 16:11:08 +0100

    add validation

--- author_quiz/app.js
+++ author_quiz/app.js
@@ -4,6 +4,9 @@
   'use strict';
 
   var Quiz = React.createClass({
+    propTypes: {
+      books: React.PropTypes.array.isRequired
+    },
     render: function () {
       return <div>
         {this.props.books.map(function (book) {
@@ -14,6 +17,9 @@
   });
 
   var Book = React.createClass({
+    propTypes: {
+      title: React.PropTypes.string.isRequired
+    },
     render: function () {
       return <div><h4>{this.props.title}</h4></div>;
     }
